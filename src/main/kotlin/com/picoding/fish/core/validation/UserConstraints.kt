dfee9a9d2bf983720b1cfcce0ffd5dcf613253eb
Wertiba <wertibarozen@gmail.com>
2026-07-27 13:51:31 +0300

    fix: field validation

--- src/main/kotlin/com/picoding/fish/core/validation/UserConstraints.kt
+++ src/main/kotlin/com/picoding/fish/core/validation/UserConstraints.kt
@@ -23,7 +23,7 @@ annotation class ValidEmail(
 @Constraint(validatedBy = [])
 @Pattern(
     regexp = "^(?=.*[A-Za-z])(?=.*\\d).+.{8,72}$",
-    message = "Password must be at least 9 characters long and contain at least one digit, uppercase and lowercase character.",
+    message = "Password must be between 8 and 72 characters long and contain at least one digit, uppercase and lowercase character.",
 )
 annotation class ValidPassword(
     val message: String = "Password must be at least 9 characters long and contain at least one digit, uppercase and lowercase character.",
@@ -35,8 +35,8 @@ annotation class ValidPassword(
 @Retention(AnnotationRetention.RUNTIME)
 @Constraint(validatedBy = [])
 @Pattern(
-    regexp = "^[а-яА-Яa-zA-Z_-]{2,200}$",
-    message = "FullName must be at least 2 characters long and contain only uppercase, lowercase and _ symbols.",
+    regexp = "^[а-яА-Яa-zA-Z0-9 _-]{2,200}$",
+    message = "FullName must be between 2 and 200 characters long and contain only uppercase, lowercase, digits, space, - and _ symbols.",
 )
 annotation class ValidFullName(
     val message: String = "FullName must be at least 2 characters long and contain only uppercase, lowercase and _ symbols.",
