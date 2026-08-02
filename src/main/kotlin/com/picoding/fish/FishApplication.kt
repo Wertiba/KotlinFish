ff1870349b393a1857b6c8aeaa504e3fe7e31257
Wertiba <wertibarozen@gmail.com>
2026-08-03 00:47:14 +0300

    fix: BA

--- src/main/kotlin/com/picoding/fish/FishApplication.kt
+++ src/main/kotlin/com/picoding/fish/FishApplication.kt
@@ -3,9 +3,10 @@ package com.picoding.fish
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [UserDetailsServiceAutoConfiguration::class])
 @ConfigurationPropertiesScan
 @EnableJpaAuditing
 class FishApplication
